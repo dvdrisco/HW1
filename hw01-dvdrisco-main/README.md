02351f5e996a6bfb100516fa19d1a60ad0e42d80
Drew Driscoll <114539744+dvdrisco@users.noreply.github.com>
2023-01-19 12:43:15 -0800

    Add files via upload

---
+++ hw01-dvdrisco-main/README.md
@@ -0,0 +1,12 @@
+# ITP 365 HW01 - Fall 2022 #
+
+## Student information... ##
+Name: Tommy Trojan
+
+Email: ttrojan@usc.edu
+
+Platform: [PC/MAC]
+
+### Notes ###
+Your notes go here.
+
---
+++ hw01-dvdrisco-main/src/fileio.cpp
@@ -0,0 +1,19 @@
+#include "fileio.h"
+
+// Read in a file and modify the inputted vector of strings
+// Use return value (true/false) to indicate success
+bool getData(const std::string& inputLocation, std::vector<std::string>& data)
+{
+	// TODO: Complete the function
+
+	return true;
+}
+
+// Output the vector of strings to the inputted file name
+// Use return value (true/false) to indicate success
+bool putData(const std::string& outputLocation, const std::vector<std::string>& data)
+{
+	// TODO: Complete the function
+
+	return true;
+}
